a0700a87c07af59c37bca3a77eda86659473f388
lee chang jin <84311301+cjllee@users.noreply.github.com>
2023-07-11 09:40:41 +0900

    Update and rename name.java to <3>name.java

--- <3>name.java
+++ <3>name.java
@@ -12,8 +12,6 @@ public class name {
 			};
 		
 		
-		
-		
 		Thread th1 = new Thread(subMain);
 		th1.setName("sub1");
 		Thread th2 = new Thread(subMain);
@@ -23,7 +21,7 @@ public class name {
         th1.start();
 		th2.start();
 		
-		Thread th = Thread.currentThread();
+		Thread th = Thread.currentThread(); 
 		th.setName("Main");
 		print();
 
@@ -31,13 +29,13 @@ public class name {
 	
 	public static void print() {
 		
-		Thread th = Thread.currentThread();
+		Thread th = Thread.currentThread(); // 현재의 thread의 정보를 얻기위한 메소드
 		
 		
 		for(int i=0; i<100; i++) {
 			
 			try {
-				Thread.sleep(20);
+				Thread.sleep(20);  // 성능이 높아서 늦추고자 사용합니다.
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
